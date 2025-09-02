4b2891b80341a467fd94f3266b8733354cdfed47
arjunbaladarshan <60723978+arjunbaladarshan@users.noreply.github.com>
2025-09-02 08:40:44 +0530

    before useEffect

---
+++ src/Layout.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+
+function Layout() {
+  return (
+    <>
+      <div class="sub-header">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 col-md-8">
+              <ul class="info">
+                <li><i class="fa fa-envelope"></i> info@company.com</li>
+                <li><i class="fa fa-map"></i> Sunny Isles Beach, FL 33160</li>
+              </ul>
+            </div>
+            <div class="col-lg-4 col-md-4">
+              <ul class="social-links">
+                <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                <li><a href="https://x.com/minthu" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>  
+      <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        
+                        <a href="index.html" class="logo">
+                            <h1>Villa</h1>
+                        </a>
+                        
+                        
+                        <ul class="nav">
+                          <li><Link to="/">Home</Link></li>
+                          <li><Link to="/subjects">Subjects</Link></li>
+                          <li><a href="property-details.html">Property Details</a></li>
+                          <li><a href="contact.html">Contact Us</a></li>
+                          <li><a href="#"><i class="fa fa-calendar"></i> Schedule a visit</a></li>
+                      </ul>   
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                        
+                    </nav>
+                </div>
+            </div>
+        </div>
+      </header>
+
+      <Outlet />
+
+
+      <footer>
+        <div class="container">
+          <div class="col-lg-8">
+            <p>Copyright © 2048 Villa Agency Co., Ltd. All rights reserved. 
+            
+            Design: <a rel="nofollow" href="https://templatemo.com" target="_blank">TemplateMo</a></p>
+          </div>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Layout
---
+++ src/Subjects.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+
+function Subjects() {
+  let [data, setData] = useState([]);
+
+  console.log("Subject component re-rendered");
+  
+  const apiUrl = "https://62d6c51451e6e8f06f12bd5d.mockapi.io/faculties";
+
+  fetch(apiUrl)
+  .then(res=>res.json())
+  .then(res=>setData(res));
+  
+
+  const foramtedData = data.map((sub, index)=>{
+    return (
+      <>
+        <div class="col-lg-4 col-md-6 align-self-center mb-30 properties-items col-md-6 adv">
+          <div class="item">
+            <a href="property-details.html"><img src="assets/images/property-01.jpg" alt="" /></a>
+            <h4><a href="property-details.html">{sub.subjectName}</a></h4>
+            <ul>
+              <li>subjectCode: <span>{sub.subjectCode}</span></li>
+              <li>subjectFaculty: <span>{sub.subjectFaculty}</span></li>
+              
+            </ul>
+          </div>
+        </div>
+      </>
+    );
+  });
+  return (
+    <div class="section properties">
+    <div class="container">
+      <ul class="properties-filter">
+        <li>
+          <a class="is_active" href="#!" data-filter="*">Show All</a>
+        </li>
+        <li>
+          <a href="#!" data-filter=".adv">Apartment</a>
+        </li>
+        <li>
+          <a href="#!" data-filter=".str">Villa House</a>
+        </li>
+        <li>
+          <a href="#!" data-filter=".rac">Penthouse</a>
+        </li>
+      </ul>
+      <div class="row properties-box">
+        {foramtedData}
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <ul class="pagination">
+            <li><a href="#">1</a></li>
+            <li><a class="is_active" href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">{">>"}</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Subjects
